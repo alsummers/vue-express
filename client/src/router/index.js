601aa5d100788603fe83ee8314bb81c5c4dffae1
Allison Summers <allisonsummers44@gmail.com>
2019-01-16 15:55:15 -0500

    router setup

---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/HelloWorld'
+import Register from '@/components/Register'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+]
+});
