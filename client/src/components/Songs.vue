4df6ac158ab62811e54544cd23e532a4a00d9d52
Allison Summers <allisonsummers44@gmail.com>
2019-01-27 13:55:34 -0500

    view song start

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,13 +2,35 @@
   <v-layout row wrap>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-        <v-btn to="/songs/create" slot="action" class="cyan accent-1" fab light medium absolute right middle>
+        <v-btn
+          to="/songs/create"
+          slot="action"
+          class="cyan accent-1"
+          fab
+          light
+          medium
+          absolute
+          right
+          middle
+        >
           <v-icon>add</v-icon>
         </v-btn>
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+        <div v-for="song in songs" :key="song.id" class="song">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">{{song.title}}</div>
+              <div class="song-artist">{{song.artist}}</div>
+              <div class="song-genre">{{song.genre}}</div>
+              <v-btn dark class="cyan darken-2" :to="{name: 'song', params: {songId: song.id}}">
+              View
+              </v-btn>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImage" />
+          
+
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -35,4 +57,22 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px
+}
+.song-artist {
+  font-size: 24px
+}
+.song-genre {
+  font-size: 18px;
+}
+.album-image{
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,7 @@ import Register from '@/components/Register'
 import Login from '@/components/Login'
 import Songs from '@/components/Songs'
 import CreateSong from '@/components/CreateSong'
+import ViewSong from '@/components/ViewSong'
 
 
 Vue.use(Router)
@@ -30,6 +31,11 @@ export default new Router({
       path: '/songs',
       name: 'songs',
       component: Songs
+    },
+    {
+      path: '/songs/:songId',
+      name: 'song',
+      component: ViewSong
     },
     {
       path: '/songs/create',
